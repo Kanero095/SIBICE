7f9606aa8465fa02b1740e8df5b9fd699ba90000
Kanero Juniar <129026887+Kanero095@users.noreply.github.com>
2025-12-10 13:53:01 +0700

    Update

--- resources/views/components/copyright.blade.php
+++ resources/views/components/copyright.blade.php
@@ -1,5 +1,5 @@
 <div class="items-center justify-between text-center bg-[#F58441] py-3 px-5">
     <span class="text-sm md:text-base text-white font-extrabold">
-        Copyright© 2024 Dinas Koperasi, Usaha Kecil & Menengah Provinsi Sumatera Selatan
+        Copyright© {{ now()->year }} Dinas Koperasi, Usaha Kecil & Menengah Provinsi Sumatera Selatan
     </span>
 </div>
